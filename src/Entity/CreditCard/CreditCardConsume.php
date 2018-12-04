be160f810d6c7c31b58cd388a0598fe76675572f
jleon <jleon@optimeconsulting.com.co>
2018-12-04 16:58:43 -0500

    Refactor creditCard

--- src/Entity/CreditCard/CreditCardConsume.php
+++ src/Entity/CreditCard/CreditCardConsume.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Entity;
+namespace App\Entity\CreditCard;
 
 use App\Entity\Security\User;
 use Doctrine\ORM\Mapping as ORM;
--- src/Repository/CreditCard/CreditCardConsumeRepository.php
+++ src/Repository/CreditCard/CreditCardConsumeRepository.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Repository;
+namespace App\Repository\CreditCard;
 
 use App\Entity\CreditCardConsume;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
