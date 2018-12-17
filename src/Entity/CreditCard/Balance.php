989fc69009178ff20ee0fb00b50c86a12e9b6562
jleon <jlp25@hotmail.com>
2018-12-17 15:08:29 -0500

    Balance sheet

--- src/Entity/CreditCard/Balance.php
+++ src/Entity/CreditCard/Balance.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Entity;
+namespace App\Entity\CreditCard;
 
 use App\Entity\CreditCard\CreditRelation;
 use Doctrine\ORM\Mapping as ORM;
@@ -23,6 +23,16 @@ class Balance
      */
     private $CreditRelation;
 
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $debt;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private $payed;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -36,6 +46,30 @@ class Balance
     public function setCreditRelation(CreditRelation $CreditRelation): self
     {
         $this->CreditRelation = $CreditRelation;
+
+        return $this;
+    }
+
+    public function getDebt(): ?float
+    {
+        return $this->debt;
+    }
+
+    public function setDebt(float $debt): self
+    {
+        $this->debt = $debt;
+
+        return $this;
+    }
+
+    public function getPayed(): ?float
+    {
+        return $this->payed;
+    }
+
+    public function setPayed(?float $payed): self
+    {
+        $this->payed = $payed;
 
         return $this;
     }
