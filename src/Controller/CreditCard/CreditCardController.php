523abee7b8d133e2add8c9c8b2174e8dc684e0fe
jleon <jlp25@hotmail.com>
2019-04-06 12:21:41 -0500

    Mueve creditCard new al controlodaor de credit card

--- src/Controller/CreditCard/CreditCardController.php
+++ src/Controller/CreditCard/CreditCardController.php
@@ -9,12 +9,41 @@
 namespace App\Controller\CreditCard;
 
 
+use App\Entity\CreditCard\CreditCard;
+use App\Form\Credit\CreditCardType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/credit-card")
+ * */
 class CreditCardController extends Controller
 {
-    public function list()
-    {
 
+    /**
+     * @Route("/new", name="credit_card_new")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function createCreditCard(Request $request)
+    {
+        $creditCard = new CreditCard();
+        $form = $this->createForm(CreditCardType::class, $creditCard);
+
+        $form->handleRequest($request);
+
+        if ( $form->isSubmitted() && $form->isValid() ){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($creditCard);
+            $em->flush();
+
+            $this->addFlash('success', 'Tarjeta de crédito creada');
+            $this->redirectToRoute('credit');
+        }
+
+        return $this->render('credit/credit_card_new.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
--- src/Controller/CreditCard/CreditController.php
+++ src/Controller/CreditCard/CreditController.php
@@ -111,29 +111,4 @@ class CreditController extends Controller
 //
 //    }
 
-    /**
-     * @Route("/credit-card-new", name="credit_card_new")
-     * @param Request $request
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function createCreditCard(Request $request)
-    {
-        $creditCard = new CreditCard();
-        $form = $this->createForm(CreditCardType::class, $creditCard);
-
-        $form->handleRequest($request);
-
-        if ( $form->isSubmitted() && $form->isValid() ){
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($creditCard);
-            $em->flush();
-
-            $this->addFlash('success', 'Tarjeta de crédito creada');
-            $this->redirectToRoute('credit');
-        }
-
-        return $this->render('credit/credit_card_new.html.twig', [
-            'form' => $form->createView()
-        ]);
-    }
 }
