2aaddabc59001cfe6e5414684b517cfba3e638fb
jleon <jlp25@hotmail.com>
2019-04-21 12:16:53 -0500

    formulario credit card user

--- src/Controller/CreditCard/CreditCardUserController.php
+++ src/Controller/CreditCard/CreditCardUserController.php
@@ -9,17 +9,22 @@
 namespace App\Controller\CreditCard;
 
 use App\Entity\CreditCard\CreditCardConsume;
+use App\Entity\CreditCard\CreditCardUser;
 use App\Extractor\CreditCard\CreditCardConsumeExtractor;
+use App\Form\Credit\CreditCardUserType;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/card_consume")
+ * @Route("/carduser")
  * */
 class CreditCardUserController extends Controller
 {
     /**
-     * @Route("/{user}")
+     * @Route("/user/{user}")
      * @param $user
      * @param CreditCardConsumeExtractor $cardConsumeExtractor
      */
@@ -28,4 +33,35 @@ class CreditCardUserController extends Controller
 //        $cardConsumeExtractor->get;
 //        $this->getDoctrine()->getRepository(CreditCardConsume::class)->get
     }
+
+    /**
+     * @Route("/new", name="card_user_new")
+     *
+     * @param Request $request
+     * @return Response
+     * @throws Exception
+     */
+    public function new(Request $request)
+    {
+        $cardUser = new CreditCardUser();
+        $cardUser->setParent($this->getUser());
+        $form = $this->createForm(CreditCardUserType::class, $cardUser);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($cardUser);
+            $em->flush();
+
+            $this->addFlash('success', 'Credit User created');
+            $this->redirectToRoute('card_user_new');
+        }
+
+        return $this->render('credit/card_user_new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+
 }
