17194078d177595d9d763266976e645a14513dea
jleon <jlp25@hotmail.com>
2019-06-09 14:55:30 -0500

    Controller payment

--- src/Controller/CreditCard/CardConsumeController.php
+++ src/Controller/CreditCard/CardConsumeController.php
@@ -8,11 +8,12 @@ use App\Entity\CreditCard\CreditCardConsume;
 use App\Entity\CreditCard\CreditCardPayments;
 use App\Entity\CreditCard\CreditCardUser;
 use App\Extractor\CreditCard\CreditCardConsumeExtractor;
-use App\Form\Credit\CreditPaymentType;
+use App\Form\Credit\CreditCardPaymentType;
 use App\Service\CreditCard\CreditCardConsumeProvider;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -33,7 +34,11 @@ class CardConsumeController extends Controller
     public function activateConsumeAction(CreditCardConsume $consume)
     {
         try{
-            $consume->activate();
+            if  ($consume->getStatus() == CreditCardConsume::STATUS_CREATED){
+                $consume->activate();
+            }else{
+                $consume->setStatus(CreditCardConsume::STATUS_CREATED);
+            }
             $em = $this->getDoctrine()->getManager();
             $em->persist($consume);
             $em->flush();
@@ -68,24 +73,46 @@ class CardConsumeController extends Controller
 
 
     /**
-     * @Route("/payment/{cardConsume}")
+     * @Route("/payment/{cardConsume}", name="pay_consume")
      * @param CreditCardConsume $cardConsume
      * @param CreditCardConsumeExtractor $consumeExtractor
+     * @param Request $request
      * @return Response
      */
     public function paymentAction(
         CreditCardConsume $cardConsume,
-        CreditCardConsumeExtractor $consumeExtractor
+        CreditCardConsumeExtractor $consumeExtractor,
+        Request $request
     )
     {
+        dump($cardConsume);
         $payment = new CreditCardPayments();
-        $payment->setAmount($consumeExtractor->extractNextPaymentAmount($cardConsume));
 
-        $form = $this->createForm(CreditPaymentType::class, $payment);
+        $isPost = $this->isPost($request);
+        if (!$isPost){
+            $payment->setAmount($consumeExtractor->extractNextPaymentAmount($cardConsume));
+        }
+        $form = $this->createForm(CreditCardPaymentType::class, $payment);
 
-        return $this->render('', [
-            'form' => $form->createView()
+        if ($isPost){
+            $form->handleRequest();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($payment);
+            $em->flush();
+        }
+
+        return $this->render('credit/new_card_payment.html.twig', [
+            'form' => $form->createView() ?? null
         ]);
+    }
+
+    /**
+     * @param Request $request
+     * @return bool
+     */
+    private function isPost(Request $request): bool
+    {
+        return $request->isMethod('POST');
     }
 
 }
