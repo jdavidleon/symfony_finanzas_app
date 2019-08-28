d14ba1678dda1bd884eb441b974c4795d0746226
jleon <jlp25@hotmail.com>
2019-08-28 15:18:13 -0500

    Some CredicarConsumeextractor tests

--- tests/Extractor/CreditCard/CreditCardConsumeExtractorTest.php
+++ tests/Extractor/CreditCard/CreditCardConsumeExtractorTest.php
@@ -9,6 +9,8 @@ use App\Extractor\CreditCard\CreditCardConsumeExtractor;
 use App\Repository\CreditCard\CreditCardPaymentsRepository;
 use App\Service\CreditCard\CreditCalculations;
 use App\Service\CreditCard\CreditCardConsumeProvider;
+use Exception;
+use PHPUnit\Framework\MockObject\Builder\InvocationMocker;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
@@ -17,69 +19,128 @@ class CreditCardConsumeExtractorTest extends TestCase
     private $consumeExtractor;
 
     /**
-     * @var MockObject
+     * @var MockObject|CreditCalculations
      */
     private $calculations;
 
+    /**
+     * @var MockObject|CreditCardPaymentsRepository
+     */
+    private $paymentsRepository;
+
+    private $creditCardConsume;
+
+    /**
+     * @var MockObject
+     * */
+    private $cardConsumeProvider;
+
+    /**
+     * @throws Exception
+     */
     public function setUp(): void
     {
-        $cardConsumeProvider = $this->createMock(CreditCardConsumeProvider::class);
-        $paymentsRepository = $this->createMock(CreditCardPaymentsRepository::class);
+        $this->cardConsumeProvider = $this->createMock(CreditCardConsumeProvider::class);
+        $this->paymentsRepository = $this->createMock(CreditCardPaymentsRepository::class);
         $this->calculations = $this->createMock(CreditCalculations::class);
         $this->consumeExtractor = new CreditCardConsumeExtractor(
-            $cardConsumeProvider,
-            $paymentsRepository,
+            $this->cardConsumeProvider,
+            $this->paymentsRepository,
             $this->calculations
         );
+
+        $this->creditCardConsume = $this->creditCardConsumeObject();
     }
 
-//    public function testExtractActualDebt()
-//    {
-//        $cardConsume = new CreditCardConsume();
-//        $cardConsume->setAmount(2000);
-//        $cardConsume->setAmountPayed(1200);
-//
-//        $this->calculations
-//            ->expects(self::once())
-//            ->method('calculateActualCreditCardConsumeDebt')
-//            ->with(2000, 1200)
-//            ->willReturn((float)800);
-//
-//        $consumeExtractor = $this->consumeExtractor->extractActualDebt(
-//            $cardConsume
-//        );
-//
-//        self::assertSame(800, $consumeExtractor);
-//    }
-
-    public function testExtractNextCapitalAmount()
+    /**
+     * @return float
+     * @throws Exception
+     */
+    public function testExtractActualDebt(): float
     {
-        $creditCardConsume = new CreditCardConsume();
-        $creditCardConsume->setAmount(5000);
-        $creditCardConsume->setAmountPayed(1000);
-        $creditCardConsume->setDues(10);
-        $creditCardConsume->setDuesPayed(2);
+        $this->calculateConsumeActualDebtReturn();
 
-        $this->calculations
-            ->expects(self::once())
-            ->method('calculateActualCreditCardConsumeDebt')
-            ->with(5000, 1000)
-            ->willReturn((float)4000);
+        $actualDebt = $this->consumeExtractor->extractActualDebt(
+            $this->creditCardConsume
+        );
 
-        $this->calculations
-            ->expects(self::once())
-            ->method('calculateNumberOfPendingDues')
-            ->with(10, 2)
-            ->willReturn(8);
+        self::assertEquals(800, $actualDebt);
+
+        return $actualDebt;
+    }
+
+    /**
+     * @return int
+     * @throws Exception
+     */
+    public function testExtractPendingDues(): int
+    {
+        $this->numberOfPendingDuesReturn();
+
+        $pendingDues = $this->consumeExtractor->extractPendingDues(
+            $this->creditCardConsume
+        );
+
+        self::assertSame(8, $pendingDues);
+
+        return $pendingDues;
+    }
+
+    /**
+     * @param float $actualDebt
+     * @param int $pendingDues
+     * @throws Exception
+     *
+     * @depends testExtractActualDebt
+     * @depends testExtractPendingDues
+     */
+    public function testExtractNextCapitalAmount(float $actualDebt, int $pendingDues)
+    {
+        $this->calculateConsumeActualDebtReturn();
+        $this->numberOfPendingDuesReturn();
 
         $this->calculations
             ->expects(self::once())
             ->method('calculateNextCapitalAmount')
-            ->with((float)4000, 8)
-            ->willReturn((float)500);
+            ->with($actualDebt, $pendingDues)
+            ->willReturn((float)100);
 
-        $consumeExtractor = $this->consumeExtractor->extractNextCapitalAmount($creditCardConsume);
+        $consumeExtractor = $this->consumeExtractor->extractNextCapitalAmount(
+            $this->creditCardConsume
+        );
 
-        self::assertSame((float)500, $consumeExtractor);
+        self::assertEquals(100, $consumeExtractor);
+    }
+
+    private function calculateConsumeActualDebtReturn(): void
+    {
+        $this->calculations
+            ->expects(self::once())
+            ->method('calculateActualCreditCardConsumeDebt')
+            ->with($this->creditCardConsume->getAmount(), $this->creditCardConsume->getAmountPayed())
+            ->willReturn((float)800);
+    }
+
+    private function numberOfPendingDuesReturn(): void
+    {
+        $this->calculations
+            ->expects(self::once())
+            ->method('calculateNumberOfPendingDues')
+            ->with($this->creditCardConsume->getDues(), $this->creditCardConsume->getDuesPayed())
+            ->willReturn(8);
+    }
+
+    /**
+     * @return CreditCardConsume
+     * @throws Exception
+     */
+    private function creditCardConsumeObject(): CreditCardConsume
+    {
+        $creditCardConsume = new CreditCardConsume();
+        $creditCardConsume->setAmount(2000);
+        $creditCardConsume->setAmountPayed(1200);
+        $creditCardConsume->setDues(10);
+        $creditCardConsume->setDuesPayed(2);
+        return $creditCardConsume;
     }
 }
