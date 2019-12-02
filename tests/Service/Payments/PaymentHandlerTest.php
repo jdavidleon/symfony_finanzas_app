31d893105e4de6d8b4b9b8681b40ae8ba4e8e5b0
jleon <jlp25@hotmail.com>
2019-12-01 21:35:35 -0500

    Agregamos test a los pagos basicos de usuario y tarjeta

--- tests/Service/Payments/PaymentHandlerTest.php
+++ tests/Service/Payments/PaymentHandlerTest.php
@@ -4,17 +4,21 @@
 namespace App\Tests\Service\Payments;
 
 
+use App\Entity\CreditCard\CreditCard;
 use App\Entity\CreditCard\CreditCardConsume;
 use App\Entity\CreditCard\CreditCardPayment;
+use App\Entity\CreditCard\CreditCardUser;
 use App\Entity\CreditCard\Model\ConsumePaymentResume;
 use App\Exception\ExcedeAmountDebtException;
 use App\Exception\MinimalAmountPaymentRequiredException;
 use App\Extractor\CreditCard\CreditCardConsumeExtractor;
 use App\Factory\Payments\CreditCardPaymentFactory;
+use App\Repository\CreditCard\CreditCardConsumeRepository;
 use App\Service\Payments\PaymentHandler;
 use Doctrine\ORM\EntityManager;
 use Exception;
 use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
 
 class PaymentHandlerTest extends TestCase
 {
@@ -115,17 +119,7 @@ class PaymentHandlerTest extends TestCase
         self::assertEquals(300, $consume->getAmountPayed());
         self::assertEquals(3, $consume->getDuesPayed());
 
-        /** @var ConsumePaymentResume $due */
-        foreach ($dues as $key => $due) {
-            /** @var CreditCardPayment $payment */
-            $payment = $consume->getPayments()->get($key);
-            self::assertEquals($due->getCapitalAmount(), $payment->getCapitalAmount());
-            self::assertEquals($due->getInterest(), $payment->getInterestAmount());
-            self::assertEquals($due->getCapitalAmount(), $payment->getRealCapitalAmount());
-            self::assertEquals($due->getDueNumber(), $payment->getDue());
-            self::assertTrue($payment->isLegalDue());
-            self::assertEquals($due->getPaymentMonth(), $payment->getMonthPayed());
-        }
+        $this->assertPaymentConsume($consume, $dues);
 
         $this->em->persist($consume)->shouldBeCalledTimes(3);
         $this->em->flush()->shouldBeCalled();
@@ -155,17 +149,7 @@ class PaymentHandlerTest extends TestCase
         self::assertEquals(1212500, $consume->getAmountPayed());
         self::assertEquals(2, $consume->getDuesPayed());
 
-        /** @var ConsumePaymentResume $due */
-        foreach ($dues as $key => $due) {
-            /** @var CreditCardPayment $payment */
-            $payment = $consume->getPayments()->get($key);
-            self::assertEquals($due->getCapitalAmount(), $payment->getCapitalAmount());
-            self::assertEquals($due->getInterest(), $payment->getInterestAmount());
-            self::assertEquals($due->getCapitalAmount(), $payment->getRealCapitalAmount());
-            self::assertEquals($due->getDueNumber(), $payment->getDue());
-            self::assertTrue($payment->isLegalDue());
-            self::assertEquals($due->getPaymentMonth(), $payment->getMonthPayed());
-        }
+        $this->assertPaymentConsume($consume, $dues);
 
         $noLegalPayment = $consume->getPayments()->get(2);
         self::assertEquals(212500, $noLegalPayment->getCapitalAmount());
@@ -177,6 +161,68 @@ class PaymentHandlerTest extends TestCase
 
         $this->em->persist($consume)->shouldBeCalledTimes(3);
         $this->em->flush()->shouldBeCalled();
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testProcessAllPaymentsByCardAndUserIfThereAreNotConsumes()
+    {
+        $consumeRepo = $this->prophesize(CreditCardConsumeRepository::class);
+        $this->em->getRepository(CreditCardConsume::class)->willReturn($consumeRepo);
+
+        $creditCard = new CreditCard();
+        $user = new CreditCardUser();
+        $consumeRepo->getByCardAndUser($creditCard, $user)->willReturn([]);
+
+        $this->em->persist(Argument::any())->shouldNotBeCalled();
+        $this->em->flush()->shouldBeCalled();
+
+        $this->paymentHandler->processAllPaymentsByCardAndUser($creditCard, $user);
+    }
+
+    /**
+     * @throws Exception
+     * */
+    public function testProcessAllPaymentsByCardAndUserWhenExistConsumesToPay()
+    {
+        $consumeRepo = $this->prophesize(CreditCardConsumeRepository::class);
+        $this->em->getRepository(CreditCardConsume::class)->willReturn($consumeRepo);
+
+        $creditCard = new CreditCard();
+        $user = new CreditCardUser();
+
+        $consume1 = $this->consumeObject(45000, 2.3, 8, '2019-10');
+        $due1 = $this->createConsumePaymentResume(1, 5625, 1035, '2019-10');
+        $due2 = $this->createConsumePaymentResume(2, 5625, 906, '2019-11');
+        $dues1 = [$due1, $due2];
+
+        $consume2 = $this->consumeObject(780000, 1.9, 5, '2019-09');
+        $due3 = $this->createConsumePaymentResume(1, 156000, 14820, '2019-09');
+        $due4 = $this->createConsumePaymentResume(2, 156000, 12856, '2019-10');
+        $due5 = $this->createConsumePaymentResume(3, 156000, 8892, '2019-11');
+        $dues2 = [$due3, $due4, $due5];
+
+        $consumes = [$consume1, $consume2];
+        $consumeRepo->getByCardAndUser($creditCard, $user)->willReturn($consumes);
+        $this->consumeExtractor->extractPendingPaymentsByConsume($consume1, true)->willReturn($dues1);
+        $this->consumeExtractor->extractPendingPaymentsByConsume($consume2, true)->willReturn($dues2);
+
+        $this->em->persist($consume1)->shouldBeCalledTimes(1);
+        $this->em->persist($consume2)->shouldBeCalledTimes(1);
+        $this->em->flush()->shouldBeCalled();
+
+        $this->paymentHandler->processAllPaymentsByCardAndUser($creditCard, $user);
+
+        self::assertEquals(2, $consume1->getDuesPayed());
+        self::assertCount(2, $consume1->getPayments());
+        self::assertEquals(11250, $consume1->getAmountPayed());
+        self::assertCount(3, $consume2->getPayments());
+        self::assertEquals(468000, $consume2->getAmountPayed());
+        self::assertEquals(3, $consume2->getDuesPayed());
+
+        $this->assertPaymentConsume($consume1, $dues1);
+        $this->assertPaymentConsume($consume2, $dues2);
     }
 
     /**
@@ -216,5 +262,24 @@ class PaymentHandlerTest extends TestCase
             $interest,
             $monthPayed
         );
+    }
+
+    /**
+     * @param array $dues
+     * @param CreditCardConsume $consume
+     */
+    private function assertPaymentConsume(CreditCardConsume $consume, array $dues): void
+    {
+        /** @var ConsumePaymentResume $due */
+        foreach ($dues as $key => $due) {
+            /** @var CreditCardPayment $payment */
+            $payment = $consume->getPayments()->get($key);
+            self::assertEquals($due->getCapitalAmount(), $payment->getCapitalAmount());
+            self::assertEquals($due->getInterest(), $payment->getInterestAmount());
+            self::assertEquals($due->getCapitalAmount(), $payment->getRealCapitalAmount());
+            self::assertEquals($due->getDueNumber(), $payment->getDue());
+            self::assertTrue($payment->isLegalDue());
+            self::assertEquals($due->getPaymentMonth(), $payment->getMonthPayed());
+        }
     }
 }
